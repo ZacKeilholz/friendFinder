4db8012463c5b5e51137466acf6b961edfe412d5
zac keilholz <keilh001@umn.edu>
2018-06-12 21:05:20 -0500

    Initial Commit

---
+++ data/friends.js
@@ -0,0 +1,16 @@
+// ===============================================================================
+// DATA
+// Initial Friend Data
+// ===============================================================================
+
+var friendsData = [
+  {
+    name: "Ahmed",
+    photoURL: "ahmed@example.com",
+    customerID: "afhaque89",
+    phoneNumber: "000-000-0000"
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsData;
